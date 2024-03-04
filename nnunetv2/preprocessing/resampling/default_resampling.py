35753da2a4092090bb4527754ccf57c48c45e300
eolika <romaactor@gmail.com>
2024-03-04 11:07:59 +0300

    add no_resampling_data_or_seg_to_shape function

--- nnunetv2/preprocessing/resampling/default_resampling.py
+++ nnunetv2/preprocessing/resampling/default_resampling.py
@@ -214,3 +214,18 @@ def resample_data_or_seg(data: np.ndarray, new_shape: Union[Tuple[float, ...], L
     else:
         # print("no resampling necessary")
         return data
+    
+
+def no_resampling_data_or_seg_to_shape(data: Union[torch.Tensor, np.ndarray],
+                                       new_shape: Union[Tuple[int, ...], List[int], np.ndarray],
+                                       current_spacing: Union[Tuple[float, ...], List[float], np.ndarray],
+                                       new_spacing: Union[Tuple[float, ...], List[float], np.ndarray],
+                                       is_seg: bool = False,
+                                       order: int = 3, order_z: int = 0,
+                                       force_separate_z: Union[bool, None] = False,
+                                       separate_z_anisotropy_threshold: float = ANISO_THRESHOLD):
+    """
+    Hacky resampling function which actually doesn't perform any resampling.
+    Way easier to implement no resampling training/inference this way, trust me.
+    """
+    return data
