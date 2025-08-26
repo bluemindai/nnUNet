bac63e49b34c51e83d23251f962689d65d66d76b
Roman Fitzjalen <romaactor@gmail.com>
2025-08-26 12:37:58 +0300

    fix tr

--- nnunetv2/training/nnUNetTrainer/variants/custom/customTrainersMOSAIC.py
+++ nnunetv2/training/nnUNetTrainer/variants/custom/customTrainersMOSAIC.py
@@ -5,142 +5,142 @@ from nnunetv2.training.nnUNetTrainer.variants.data_augmentation.nnUNetTrainerNoM
 from nnunetv2.training.nnUNetTrainer.variants.loss.nnUNetTrainerDiceLoss import *
 
 class nnUNetTrainer_MOSAIC_3k(nnUNetTrainer):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.num_epochs = 3000
 
 class nnUNetTrainer_MOSAIC_3k_HalfLR(nnUNetTrainer_MOSAIC_3k):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.initial_lr = 5e-3
 
 class nnUNetTrainer_MOSAIC_3k_QuarterLR(nnUNetTrainer_MOSAIC_3k):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.initial_lr = 2.5e-3
 
 class nnUNetTrainer_MOSAIC_2k(nnUNetTrainer):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.num_epochs = 2000
 
 class nnUNetTrainer_MOSAIC_2k_HalfLR(nnUNetTrainer_MOSAIC_2k):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.initial_lr = 5e-3
 
 class nnUNetTrainer_MOSAIC_2k_QuarterLR(nnUNetTrainer_MOSAIC_2k):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.initial_lr = 2.5e-3
 
 class nnUNetTrainer_MOSAIC_1_5k(nnUNetTrainer):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.num_epochs = 1500
 
 class nnUNetTrainer_MOSAIC_1_5k_NoMirroring(nnUNetTrainerNoMirroring):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.num_epochs = 1500
 
 class nnUNetTrainer_MOSAIC_1_5k_HalfLR(nnUNetTrainer_MOSAIC_1_5k):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.initial_lr = 5e-3
 
 class nnUNetTrainer_MOSAIC_1_5k_HalfLR_NoMirroring(nnUNetTrainer_MOSAIC_1_5k_NoMirroring):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.initial_lr = 5e-3
 
 class nnUNetTrainer_MOSAIC_1_5k_QuarterLR(nnUNetTrainer_MOSAIC_1_5k):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.initial_lr = 2.5e-3
 
 class nnUNetTrainer_MOSAIC_1_5k_QuarterLR_NoMirroring(nnUNetTrainer_MOSAIC_1_5k_NoMirroring):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.initial_lr = 2.5e-3
 
 class nnUNetTrainer_MOSAIC_1k_HalfLR(nnUNetTrainer):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.initial_lr = 5e-3
 
 class nnUNetTrainer_MOSAIC_1k_HalfLR_NoMirroring(nnUNetTrainerNoMirroring):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.initial_lr = 5e-3
 
 class nnUNetTrainer_MOSAIC_1k_QuarterLR_NoMirroring(nnUNetTrainerNoMirroring):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.initial_lr = 2.5e-3
 
 class nnUNetTrainer_MOSAIC_1k_QuarterLR(nnUNetTrainer):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.initial_lr = 2.5e-3
 
 class nnUNetTrainer_MOSAIC_500_HalfLR(nnUNetTrainer):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.initial_lr = 5e-3
         self.num_epochs = 500
 
 class nnUNetTrainer_MOSAIC_500_QuarterLR(nnUNetTrainer):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.initial_lr = 2.5e-3
         self.num_epochs = 500
 
 class nnUNetTrainer_MOSAIC_500_QuarterLR_NoMirroring(nnUNetTrainerNoMirroring):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__plans, configuration, fold, dataset_json, device)
         self.initial_lr = 2.5e-3
         self.num_epochs = 500
 
 class nnUNetTrainer_MOSAIC_250_QuarterLR(nnUNetTrainer):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.initial_lr = 2.5e-3
         self.num_epochs = 250
 
 
 class nnUNetTrainer_MOSAIC_250_QuarterLR_NoMirroring(nnUNetTrainerNoMirroring):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.initial_lr = 2.5e-3
         self.num_epochs = 250
         
 
 class nnUNetTrainer_MOSAIC_500_QuarterLR_NoMirroring_noSmooth(nnUNetTrainerDiceCELoss_noSmooth):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.initial_lr = 2.5e-3
         self.num_epochs = 500
 
@@ -153,9 +153,9 @@ class nnUNetTrainer_MOSAIC_500_QuarterLR_NoMirroring_noSmooth(nnUNetTrainerDiceC
 
 
 class nnUNetTrainer_MOSAIC_1k_NoMirroring_noSmooth(nnUNetTrainerDiceCELoss_noSmooth):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
 
         def configure_rotation_dummyDA_mirroring_and_inital_patch_size(self):
             rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes = \
@@ -166,8 +166,8 @@ class nnUNetTrainer_MOSAIC_1k_NoMirroring_noSmooth(nnUNetTrainerDiceCELoss_noSmo
 
 
 class nnUNetTrainer_MOSAIC_500_HalfLR_NoMirroring(nnUNetTrainerNoMirroring):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, 
                  device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.initial_lr = 5e-3
         self.num_epochs = 500
